0598aaa93c3105fab04a9958115434822babdfcb
刘宜海 <shliuyihai@126.com>
2025-07-18 17:03:53 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,8 @@
+## Wallpaper
+自动下载bing壁纸，默认下载4k画质，接口由[bing.img.run](https://bing.img.run)提供 <br>
+<br>
+没事自己捣鼓着玩儿
+<br>
+在桌面创建todo.txt，输入待办事项，可在壁纸上显示代办，不创建则默认摸鱼
+<br>
+默认1分钟更换一次壁纸
---
+++ WallpaperForm/MainForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WallpaperForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            label1 = new Label();
+            wallpaperDownloader = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            todoReader = new System.Windows.Forms.Timer(components);
+            LogTextBox = new RichTextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(0, 1);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 17);
+            label1.TabIndex = 1;
+            label1.Text = "运行日志";
+            // 
+            // wallpaperDownloader
+            // 
+            wallpaperDownloader.Interval = 60000;
+            wallpaperDownloader.Tick += DownLoadBingWallpaper;
+            // 
+            // todoReader
+            // 
+            todoReader.Interval = 10000;
+            todoReader.Tick += TextOnWallpaper;
+            // 
+            // LogTextBox
+            // 
+            LogTextBox.Location = new Point(0, 21);
+            LogTextBox.Name = "LogTextBox";
+            LogTextBox.Size = new Size(411, 397);
+            LogTextBox.TabIndex = 2;
+            LogTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(411, 419);
+            Controls.Add(LogTextBox);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "MainForm";
+            Text = "Bing壁纸";
+            Deactivate += Form1_Deactivate;
+            Load += MainForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+
+
+        }
+        // Fix for CS0123: Ensure the method signature matches the EventHandler delegate
+        // Change the method signature of DownloadWallpaperAsync to match the EventHandler delegate
+
+        #endregion
+        private Label label1;
+        private System.Windows.Forms.Timer wallpaperDownloader;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer todoReader;
+        private RichTextBox LogTextBox;
+    }
+}
